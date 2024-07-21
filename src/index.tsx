ca762a2973dafc77d7e332a2747189d24a3106c7
DGUHJH <ghkdwogud852@gmail.com>
2024-07-21 16:18:01 +0900

    init

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import 'antd/dist/reset.css'
+import { MainPage } from 'pages/Main'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'styles/global.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+)
