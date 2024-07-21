56755aea19e7a67de3780b63e8286e8e3db65721
DGUHJH <ghkdwogud852@gmail.com>
2024-07-22 03:43:19 +0900

    feat : RequestInfoPage 추가

---
+++ src/pages/Request/Info/RequestInfoPage.tsx
@@ -0,0 +1,96 @@
+import { Root } from 'components/Root'
+import { TabBar } from 'components/TabBar'
+import { STORE_LIST } from 'constants/system/store'
+import { FC } from 'react'
+import {
+  Container,
+  ContentButton,
+  ContentContainer,
+  ContentInput,
+  ContentInputCaptionTypo,
+  ContentInputCheckbox,
+  ContentInputContainer,
+  ContentInputTitleTypo,
+  ContentInputTitleTypoContainer,
+  ContentInputWrapper,
+  ContentSelect,
+  ContentTextArea,
+  ContentTimePicker,
+  SubtitleTypo,
+  TitleTypo,
+} from './styled'
+
+type RequestInfoPageProps = {
+  className?: string
+}
+
+export const RequestInfoPage: FC<RequestInfoPageProps> = ({ className }) => {
+  return (
+    <Root className={className}>
+      <Container>
+        <TitleTypo>배달 요청하기</TitleTypo>
+        <SubtitleTypo>어떤 음식을 배달시킬까요?</SubtitleTypo>
+        <ContentContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypo>가게 선택하기</ContentInputTitleTypo>
+            <ContentInputWrapper>
+              <ContentSelect showSearch placeholder="가게를 선택해주세요." options={STORE_LIST} />
+            </ContentInputWrapper>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypo>목적지 선택하기</ContentInputTitleTypo>
+            <ContentInputWrapper>
+              <ContentInput placeholder="목적지를 입력해주세요." />
+            </ContentInputWrapper>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypoContainer>
+              <ContentInputTitleTypo>정기권 여부</ContentInputTitleTypo>
+              <ContentInputCheckbox />
+            </ContentInputTitleTypoContainer>
+            <ContentInputCaptionTypo>정기권은 매주 똑같은 주문을 하는 방식입니다.</ContentInputCaptionTypo>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypoContainer>
+              <ContentInputTitleTypo>도착 시간</ContentInputTitleTypo>
+            </ContentInputTitleTypoContainer>
+            <ContentTimePicker showMinute showSecond={false} format={'HH:MM'} minuteStep={5} />
+            <ContentInputCaptionTypo>실제 도착은 입력한 시간의 10분 내외로 도착합니다.</ContentInputCaptionTypo>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypo>메뉴 이름</ContentInputTitleTypo>
+            <ContentInputWrapper>
+              <ContentTextArea placeholder="메뉴 이름을 입력해주세요." autoSize={{ minRows: 2, maxRows: 3 }} />
+            </ContentInputWrapper>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypo>배달 금액</ContentInputTitleTypo>
+            <ContentInputWrapper>
+              <ContentSelect
+                showSearch
+                placeholder="배달 금액을 선택해주세요."
+                options={[
+                  { label: '1,000원', value: '1,000원' },
+                  { label: '1,500원', value: '1,500원' },
+                  { label: '2,000원', value: '2,000원' },
+                  { label: '2,500원', value: '2,500원' },
+                  { label: '3,000원', value: '3,000원' },
+                ]}
+              />
+            </ContentInputWrapper>
+          </ContentInputContainer>
+          <ContentInputContainer>
+            <ContentInputTitleTypo>기타 요청사항</ContentInputTitleTypo>
+            <ContentInputWrapper>
+              <ContentTextArea placeholder="기타 요청사항을 입력해주세요." autoSize={{ minRows: 2, maxRows: 3 }} />
+            </ContentInputWrapper>
+          </ContentInputContainer>
+          <ContentButton type={'primary'} size={'large'}>
+            요청하기
+          </ContentButton>
+        </ContentContainer>
+      </Container>
+      <TabBar />
+    </Root>
+  )
+}
