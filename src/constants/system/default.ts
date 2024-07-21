9e4bf6aecf63d2d47d98975c2a25dbfc3525a791
DGUHJH <ghkdwogud852@gmail.com>
2024-07-21 19:43:10 +0900

    chore : PRIMARY_COLOR_2 추가

--- src/constants/system/default.ts
+++ src/constants/system/default.ts
@@ -3,3 +3,4 @@ export const MOBILE_MIN_WIDTH = 360
 export const MOBILE_MAX_WIDTH = 420
 export const HEADER_HEIGHT = 80
 export const PRIMARY_COLOR = '#ffbb8a'
+export const PRIMARY_COLOR_2 = '#32A15F'
